1dc3de6b8e6088e27f9f9bc19f29555c5ada3c6a
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2025-07-24 18:59:57 +0700

    fix: Update title in PortfolioData to reflect correct job title

--- lib/data/portfolio_data.dart
+++ lib/data/portfolio_data.dart
@@ -1,7 +1,7 @@
 class PortfolioData {
   static const String name = "Dimas Arya Murdiyan";
   static const String codeName = "DM";
-  static const String title = "Android Developer | Mobile Developer";
+  static const String title = "Android Engineer | Mobile Engineer";
   static const String location = "Jakarta, Indonesia";
   static const String email = "dimasaryamurdiyan123@gmail.com";
   static const String phone = "+6289601389327";
